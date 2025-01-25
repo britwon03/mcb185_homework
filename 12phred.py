1342567edb195f4b25283dffcd0495b54aecc973
britwon03 <ly.britney805@gmail.com>
2025-01-24 16:51:24 -0800

    update

---
+++ 12phred.py
@@ -0,0 +1,17 @@
+import math 
+def char_to_prob(char): 
+    if len(char) != 1 or not 33 <= ord(char) <= 126: 
+        return None 
+    return 10 ** (-(ord(char) - 33)/ 10)
+
+def prob_to_char(prob): 
+    if (0 < prob <= 1): 
+        q_score = -10 * math.log10(prob)
+        char = chr(round(q_score) + 33)
+        if 33 <= ord(char) <= 126: 
+            return char 
+        return None 
+   
+
+print(char_to_prob('A'))
+print(prob_to_char(0.001))
