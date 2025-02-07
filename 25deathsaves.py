8f91775117c43d9a97eb14ad80753f29791f1ea2
britwon03 <ly.britney805@gmail.com>
2025-02-06 16:56:57 -0800

    update

--- 25deathsaves.py
+++ 25deathsaves.py
@@ -1,42 +1,25 @@
-import random
+import random 
+trials = 1000000
+die = 0 
+stable = 0 
+revive = 0 
 
-def simulate_death_saves():
-    failures = 0
-    successes = 0
-    
-    while failures < 3 and successes < 3:
-        roll = random.randint(1, 20)
-        
-        if roll == 1:
-            failures += 2  
-        elif roll == 20:
-            return 'revived'  
-        elif roll < 10:
-            failures += 1
+
+for i in range(trials): 
+    failure = 0 
+    success = 0 
+    for i in range(trials): 
+        r = random.randint(1,20)
+        if r == 1 :  failure += 2 
+        elif r <= 9: failure += 1 
+        elif r <= 19: success += 1
         else:
-            successes += 1
-        
-        if failures >= 3:
-            return 'died'
-        if successes >= 3:
-            return 'stabilized'
-
-def estimate_probabilities(num_simulations = 100000):
-    outcomes = {'died': 0, 'stabilized': 0, 'revived': 0}
-    
-    for _ in range(num_simulations):
-        outcomes[simulate_death_saves()] += 1
-    
-    return {
-        'died': outcomes['died'] / num_simulations,
-        'stabilized': outcomes['stabilized'] / num_simulations,
-        'revived': outcomes['revived'] / num_simulations,
-    }
-
-def main():
-    probabilities = estimate_probabilities()
-    print('Died:', round(probabilities['died'], 4))
-    print('Stabilized:', round(probabilities['stabilized'], 4))
-    print('Revived:', round(probabilities['revived'], 4))
-
-main()
+            revive += 1 
+            break 
+        if success == 3: 
+            stable += 1 
+            break 
+        if failure == 3: 
+            die += 1 
+            break 
+print('die:', die/trials, 'stable:', stable/trials, 'revive:', revive/trials)
