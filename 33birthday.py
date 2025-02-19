28900a8bb3cb49565cfb105e6e876f5cfcffc3ce
britwon03 <ly.britney805@gmail.com>
2025-02-18 17:11:49 -0800

    update

--- 33birthday.py
+++ 33birthday.py
@@ -3,9 +3,9 @@ trials = 1000
 shared = 0 
 
 for t in range(trials): 
-    # does this classroom have shared birthday 
+    # does this classroom have shared birthday?  
     peeps = [] # empty list of students 
-    for i in range(23): # adding 19 students to list 
+    for i in range(23): # adding 23 students to list 
         bday = random.randint(0,354)
         if bday in peeps: 
             shared += 1 
