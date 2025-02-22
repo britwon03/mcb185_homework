f7147265393cb5d62e7d5abb38f3d696378190b0
britwon03 <ly.britney805@gmail.com>
2025-02-22 13:05:17 -0800

    update

--- 33birthday.py
+++ 33birthday.py
@@ -1,20 +1,25 @@
-import random 
-trials = 1000
-shared = 0 
+import random
+import sys
+
+# Read command-line arguments
+trials = int(sys.argv[1])  # Number of trials
+days = int(sys.argv[2])  # Number of days in a year
+people = int(sys.argv[3])  # Number of people in the room
+
+shared = 0  
 
 for t in range(trials): 
-    # does this classroom have shared birthday?  
-    peeps = [] # empty list of students 
-    for i in range(23): # adding 23 students to list 
-        bday = random.randint(0,354)
-        if bday in peeps: 
+    peeps = []  # Empty list of students
+    for i in range(people):  # Adding 'people' students to list 
+        bday = random.randint(0, days - 1)  # Generate birthday within range
+        if bday in peeps:  # Check for duplicates
             shared += 1 
-            break 
+            break  # Stop checking if a shared birthday is found
         else: 
-            peeps.append(bday)
+            peeps.append(bday)  # Add birthday to the list
 
+print(shared / trials)  # Probability of at least one shared birthday
 
-print(shared/trials)
 
 
 
