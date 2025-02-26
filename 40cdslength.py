f3dd52d19dc72f05cccd1e71f1d456b1b038ce99
britwon03 <ly.britney805@gmail.com>
2025-02-26 11:49:41 -0800

    new

---
+++ 40cdslength.py
@@ -0,0 +1,14 @@
+# reports length of the protein-coding genes in E.coli genome 
+
+
+import gzip 
+import sys 
+
+with gzip.open(sys.argv[1], 'rt') as fp: 
+    for line in fp: 
+        if line [0] != '#': 
+            words = line.split()
+            if words[2] == 'CDS': 
+                beg = int(words[3])
+                end = int(words[4])
+                print(end - beg + 1)
