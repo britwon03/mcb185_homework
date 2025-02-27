8b978ba68ce13aebffa3613e2cc09c999d48a1c4
britwon03 <ly.britney805@gmail.com>
2025-02-26 20:38:42 -0800

    update

--- 40cdslength.py
+++ 40cdslength.py
@@ -12,3 +12,15 @@ with gzip.open(sys.argv[1], 'rt') as fp:
                 beg = int(words[3])
                 end = int(words[4])
                 print(end - beg + 1)
+
+
+
+# does the same thing but with continue: 
+with gzip.open(sys.argv[1], 'rt') as fp: 
+    for line in fp: 
+        if line[0] == '#': continue 
+        words = line.split()
+        if words[2] != 'CDS': continue 
+        beg = int(words[3])
+        end = int(words[4])
+        print(end - beg + 1)
