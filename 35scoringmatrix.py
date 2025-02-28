6463b7f96769527d3e53111bc9f01899c10fca17
britwon03 <ly.britney805@gmail.com>
2025-02-27 21:35:07 -0800

    update

--- 35scoringmatrix.py
+++ 35scoringmatrix.py
@@ -1,21 +1,17 @@
-import sys
 
-def generate_matrix(letters, match, mismatch):
-    match, mismatch = int(match), int(mismatch)
-    print("  " + " ".join(letters))
-    for l1 in letters:
-        row_values = []
-        for l2 in letters:
-            if l1 == l2:
-                row_values.append(str(match))
-            else:
-                row_values.append(str(mismatch))
-        print(l1, " ".join(row_values))
+letters = 'ACGTNQW'
+# print header 
+print(end = '  ')
+for letter in letters: 
+    print(letter, end = ' ')
+print() # header line needs to end 
+# print matrix
+for c1 in letters: 
+    print(c1, end = ' ')
+    for c2 in letters: 
+        if c1 == c2: 
+            print('+', end = ' ')
+        else: 
+            print('-', end = ' ')
+    print()
 
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: python3 script.py <alphabet> <match> <mismatch>")
-        return
-    generate_matrix(sys.argv[1], sys.argv[2], sys.argv[3])
-
-main()
