b5d338f081c074ada6cc9b8914e41dedc21c2e8f
britwon03 <ly.britney805@gmail.com>
2025-02-28 17:04:29 -0800

    update

--- 45colorname.py
+++ 45colorname.py
@@ -5,28 +5,28 @@ R = int(sys.argv[2])
 G = int(sys.argv[3])
 B = int(sys.argv[4])
 
-def dtc(P, Q):
+def dtc(P, Q): # using taxi - cab distance to find the distance
     d = 0
     for p, q in zip(P, Q):
         d += abs(p - q)
     return d
 
-min_distance = float('inf')
+min_distance = float('inf') # set initial 
 closest_color = None
 
-with open(colorfile, 'r') as fp:
+with open(colorfile, 'rt') as fp:
     for line in fp:
         if line.startswith('#') or not line.strip():
             continue
 
         fields = line.strip().split('\t')
         name = fields[0]
-        rgb_values = fields[2].split(',')
-
+        rgb_values = fields[2].split(',') # takes the rgb values from the tsv file 
+        
         r = int(rgb_values[0])
         g = int(rgb_values[1])
         b = int(rgb_values[2])
-
+        # computes the distance between the values 
         distance = dtc([R, G, B], [r, g, b])
 
         if distance < min_distance:
