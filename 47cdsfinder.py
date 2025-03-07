cbfb1c4a475aa33446321ae777493d9c8e0efa98
britwon03 <ly.britney805@gmail.com>
2025-03-07 13:32:44 -0800

    update

--- 47cdsfinder.py
+++ 47cdsfinder.py
@@ -1,52 +1,26 @@
-import gzip
+import sequence 
+import sys 
 import mcb185
-import sys
-import sequence
-
-def find_orfs(dna, min_len=100):
-    # Find ORFs in a DNA sequence
+def find_orfs(seq, min_len):
     orfs = []
     for frame in range(3):
-        for strand in ['+', '-']:
-            if strand == '-':
-                dna = sequence.revcomp(dna)
-            protein = mcb185.translate(dna[frame:])
-            start_positions = []
-            for i in range(len(protein)):
-                if protein[i:i + 1] == 'M':
-                    start_positions.append(i)
-
-            for start in start_positions:
-                for end in range(start + min_len, len(protein)):
-                    if protein[end:end + 1] == '*':
-                        orf = protein[start:end + 1]  
-                        orf = orf.replace('*', '') + '*'  # replaces the * in the original codon files
-                        orfs.append(orf)
-                        break
+        for strand_seq in [seq, sequence.revcomp(seq)]:
+            protein = mcb185.translate(strand_seq[frame:])
+            start = None
+            for i, aa in enumerate(protein):
+                if aa == 'M' and start is None:
+                    start = i  # Mark the start of an ORF
+                elif aa == '*' and start is not None and (i - start) >= min_len:
+                    orfs.append(protein[start:i + 1])
+                    start = None  # Reset start after finding a stop codon
     return orfs
 
-
 min_len = int(sys.argv[2])
+orf_count = 0
 
-with gzip.open(sys.argv[1], 'rt') as f:
-    seq = ''
-    header = ''
-    orf_count = 0  
-    for line in f:
-        if line.startswith('>'):
-            if seq:
-                orfs = find_orfs(seq, min_len)
-                for orf in orfs:
-                    print(f'>{header}-prot-{orf_count}')  # output format
-                    print(orf)
-                    orf_count += 1
-            header = line.strip()[1:].split()[0]  # Extract only the NC_.. number
-            seq = ''
-        else:
-            seq += line.strip()
-    if seq:
-        orfs = find_orfs(seq, min_len)
-        for orf in orfs:
-            print(f'>{header}-prot-{orf_count}')
-            print(orf)
-            orf_count += 1
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+    orfs = find_orfs(seq, min_len)
+    for orf in orfs:
+        print(f'>{defline}-prot-{orf_count}')
+        print(orf)
+        orf_count += 1
