fbbcb4c88a980b8b03ce71292b08d660783aa516
britwon03 <ly.britney805@gmail.com>
2025-03-07 15:31:41 -0800

    update

--- 48transmembrane.py
+++ 48transmembrane.py
@@ -1,5 +1,6 @@
 import sys
 import gzip
+
 kd_values = {
     'A': 1.8, 'R': -4.5, 'N': -3.5, 'D': -3.5, 'C': 2.5,
     'Q': -3.5, 'E': -3.5, 'G': -0.4, 'H': -3.2, 'I': 4.5,
@@ -8,36 +9,14 @@ kd_values = {
 }
 
 def kd(seq):
-   # Calculates the average Kyte-Doolittle hydrophobicity for a sequence.
+    # Calculate KD score for a sequence
+    if len(seq) == 0: 
+        return 0 
     total_kd = 0
     for aa in seq:
-        if aa in kd_values:  # Only add if the amino acid is valid
+        if aa in kd_values:
             total_kd += kd_values[aa]
-        else:
-            total_kd += 0  # Treat unknown amino acids as 0
-    return total_kd / len(seq) if seq else 0
-
-def signal_peptide(protein):
-   # Checks for a hydrophobic signal peptide in the first 30 amino acids.
-    if len(protein) < 30:
-        return False
-    for i in range(0, 30 - 7):  # Sliding window across first 30 aa
-        region = protein[i:i+8]
-        if kd(region) >= 2.5 and 'P' not in region:
-            return True
-    return False
-
-def transmembrane_region(protein):
-    # Checks for a transmembrane region beyond the first 30 amino acids.
-    for i in range(30, len(protein) - 10):
-        region = protein[i:i+11]
-        if kd(region) >= 2.0 and 'P' not in region:
-            return True
-    return False
-
-def is_transmembrane(protein):
-    #Determines if a protein is transmembrane.
-    return signal_peptide(protein) and transmembrane_region(protein)
+    return total_kd / len(seq)
 
 # Process FASTA file
 with gzip.open(sys.argv[1], 'rt') as fp:
@@ -46,13 +25,29 @@ with gzip.open(sys.argv[1], 'rt') as fp:
     
     for line in fp:
         if line.startswith('>'):
-            if protein and is_transmembrane(protein):
-                print(header[:60])  # Truncate to match example output
-            header = line.strip()[1:]  # Remove '>'
+            if protein:
+                sp = False
+                tm = False
+                
+                # Check for signal peptide
+                for i in range(0, 30 - 7): # first 30 aa with 8 aa long 
+                    region = protein[i:i + 8]
+                    if kd(region) >= 2.5 and 'P' not in region:
+                        sp = True
+                        break
+
+                # Check for transmembrane region
+                for i in range(30, len(protein) - 10):
+                    region = protein[i:i + 11]
+                    if kd(region) >= 2.0 and 'P' not in region: # makes sure Proline is not in region 
+                        tm = True
+                        break
+
+                # If both conditions are met, print the protein header
+                if sp and tm:
+                    print(header[:60])
+
+            header = line.strip()[1:]
             protein = ''
         else:
             protein += line.strip()
-    
-    # Process last sequence
-    if protein and is_transmembrane(protein):
-        print(header[:60])
