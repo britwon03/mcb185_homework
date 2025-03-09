9208ac3fd145060f2273df98ed98b5511fdd9753
britwon03 <ly.britney805@gmail.com>
2025-03-09 16:09:21 -0700

    update

--- 47cdsfinder.py
+++ 47cdsfinder.py
@@ -10,11 +10,16 @@ for defline, seq in mcb185.read_fasta(sys.argv[1]):
         for frame in range(3):  # Three reading frames
             protein = mcb185.translate(strand_seq[frame:])
             start = None
+            
             for i, aa in enumerate(protein):
                 if aa == 'M' and start is None:
                     start = i  # Start of ORF
-                elif aa == '*' and start is not None and (i - start) >= min_len:
-                    print(f'>{defline}-prot-{orf_count}')
-                    print(protein[start:i + 1])  # Print ORF sequence
-                    orf_count += 1
+                elif aa == '*' and start is not None:
+                    orf_seq = protein[start:i]  # Exclude stop codon (*)
+
+                    if len(orf_seq) >= min_len:  
+                        print(f'>{defline}-prot-{orf_count}')
+                        print(orf_seq)  # Print ORF sequence
+                        orf_count += 1
+                    
                     start = None  # Reset for next ORF
